795223f92a27bf29cbfa1860fa289422f0b27eac
Zhincore <adam@zhincore.eu>
2024-10-09 14:26:51 +0200

    Trying to make state for the thing

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import { createIntegration, createComponent, FetchEventCallback, RuntimeContext } from "@gitbook/runtime";
+import {
+  createIntegration,
+  createComponent,
+  FetchEventCallback,
+  RuntimeContext,
+} from "@gitbook/runtime";
+import YAML from "yaml";
 
 const defaultContent = `---
 start:
@@ -22,67 +28,95 @@ pirate:
 
 type IntegrationContext = {} & RuntimeContext;
 type IntegrationBlockProps = { content: string };
-type IntegrationBlockState = { content: string };
-type IntegrationAction = { action: "click" };
+type IntegrationBlockState = { content: string; currentStep: string };
+type IntegrationAction = { action: "click"; step: string };
 
-const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (request, context) => {
+const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
+  request,
+  context
+) => {
   const { api } = context;
   const user = api.user.getAuthenticatedUser();
 
   return new Response(JSON.stringify(user));
 };
 
-const guideBlock = createComponent<IntegrationBlockProps, IntegrationBlockState, IntegrationAction, IntegrationContext>(
-  {
-    componentId: "debugguide",
-    initialState: (props) => {
-      return {
-        content: props.content || defaultContent,
-      };
-    },
-    action: async (element, action, context) => {
-      switch (action.action) {
-        case "click":
-          console.log("Button Clicked");
-          return {};
-      }
-    },
-    render: async (element, { environment }) => {
-      if (element.context.type !== "document") {
-        throw new Error("Invalid context");
-      }
-      const { editable } = element.context;
-      const { content } = element.state;
-
-      element.setCache({
-        maxAge: 86400,
-      });
-
-      const output = <text>{element.state.content}</text>;
-
-      return (
-        <block>
-          {editable ? (
-            <codeblock
-              state="content"
-              content={content}
-              syntax="yaml"
-              onContentChange={{
-                action: "@editor.node.updateProps",
-                props: {
-                  content: element.dynamicState("content"),
-                },
-              }}
-              footer={[output]}
-            />
-          ) : (
-            output
-          )}
-        </block>
-      );
-    },
+const guideBlock = createComponent<
+  IntegrationBlockProps,
+  IntegrationBlockState,
+  IntegrationAction,
+  IntegrationContext
+>({
+  componentId: "debugguide",
+  initialState: (props) => {
+    return {
+      content: props.content || defaultContent,
+      currentStep: "start",
+    };
   },
-);
+  action: async (element, action, context) => {
+    switch (action.action) {
+      case "click":
+        return { state: { ...element.state, currentStep: action.step } };
+    }
+  },
+  render: async (element, { environment }) => {
+    if (element.context.type !== "document") {
+      throw new Error("Invalid context");
+    }
+    const { context, state } = element;
+    const { editable } = context;
+    let parsedContent;
+
+    try {
+      parsedContent = JSON.parse(state.content);
+    } catch (e) {
+      // TODO:
+    }
+
+    const step = parsedContent[state.currentStep];
+
+    element.setCache({
+      maxAge: 86400,
+    });
+
+    const output = (
+      <box>
+        <markdown content={"## " + step.title} />
+        {step.buttons?.map((button) => (
+          <button
+            label={button.label}
+            onPress={{
+              action: "click",
+              step: button.target,
+            }}
+          />
+        ))}
+      </box>
+    );
+
+    return (
+      <block>
+        {editable ? (
+          <codeblock
+            state="content"
+            content={state.content}
+            syntax="yaml"
+            onContentChange={{
+              action: "@editor.node.updateProps",
+              props: {
+                content: element.dynamicState("content"),
+              },
+            }}
+            footer={[output]}
+          />
+        ) : (
+          output
+        )}
+      </block>
+    );
+  },
+});
 
 export default createIntegration({
   fetch: handleFetchEvent,
